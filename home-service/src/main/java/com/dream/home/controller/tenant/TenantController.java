565e9504142ea558b17e9db0d5cd4105bea1ee2a
jovi <506997994@qq.com>
2021-02-03 18:14:21 +0800

    新增方法

--- home-service/src/main/java/com/dream/home/controller/tenant/TenantController.java
+++ home-service/src/main/java/com/dream/home/controller/tenant/TenantController.java
@@ -4,10 +4,7 @@ import com.dream.home.common.result.CommonResult;
 import com.dream.home.entity.user.tenant.Tenant;
 import com.dream.home.service.impl.tenant.TenantServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 租户-controller
@@ -29,5 +26,17 @@ public class TenantController {
     public CommonResult<?> getById(@PathVariable("id") Integer id) {
         Tenant tenant = tenantService.getById(id);
         return CommonResult.success(tenant);
+    }
+
+    @PostMapping("/save")
+    public CommonResult<?> save(@RequestBody Tenant tenant) {
+        boolean flag = tenantService.save(tenant);
+        return CommonResult.success(flag);
+    }
+
+    @PostMapping("/updateById")
+    public CommonResult<?> updateById(@RequestBody Tenant tenant) {
+        boolean flag = tenantService.updateById(tenant);
+        return CommonResult.success(flag);
     }
 }
